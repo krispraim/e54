f8a6b3de8fdb0771b0a91a0b3b6bf129c6a3b5fe
krispraim <oseak29@gmail.com>
2023-03-01 23:33:59 +0800

    new files added

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text-plain'});
+    res.end('Hello Kode-brewers');
+}).listen(3000, '127.0.0.1');
+
+console.log('Server listening at 127.0.0.1:3000');
